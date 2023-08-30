88b5e22ab8ebea21ac0774646c52ffefbfda1328
m3ez <72428471+m3ez@users.noreply.github.com>
2023-08-31 01:13:18 +0700

    Update ofset_finder.py
    Add support for x64/x32

--- ofset_finder.py
+++ ofset_finder.py
@@ -3,38 +3,47 @@
 from __future__ import print_function
 from pwn import *
 import os
-import subprocess
-import sys  # Import the sys module
+import sys
 
-# Check if the correct number of arguments is provided
-if len(sys.argv) != 2:
-    log.warn("Usage: python script_name.py path_to_binary")
-    sys.exit(1)
+def find_offset(binary_path):
+    elf = ELF(binary_path)
 
-binary_path = sys.argv[1]  # Get the path to the binary from command-line arguments
+    with process(binary_path) as p:
+        try:
+            log.info('Sending cyclic input to trigger potential buffer overflow...')
+            p.sendline(cyclic(1000, n=8))
+            p.wait()
+            core = p.corefile
+            # Determine the stack pointer register based on the architecture
+            stack_ptr_register = 'rsp' if elf.bits == 64 else 'esp'
+            offset = cyclic_find(core.read(getattr(core, stack_ptr_register), 8), n=8)
+        except:
+            p.close()
+            p = process([binary_path,cyclic(1000, n=8)])
+            p.wait()
+            # Analyze the core dump to find the offset
+            core = p.corefile
+            offset = cyclic_find(core.read(core.rsp, 8), n=8)
 
-# Load the ELF binary
-elf = ELF(binary_path)
-
-# Start the process using a context manager
-with process(binary_path) as p:
-    # Send cyclic input to trigger potential buffer overflow
-    try:
-        p.sendline(cyclic(1000, n=8))
-        p.wait()
-        # Analyze the core dump to find the offset
-        core = p.corefile
-        offset = cyclic_find(core.read(core.rsp, 8), n=8)
-    except:
-        p.close()
-        p = process([binary_path,cyclic(1000, n=8)])
-        p.wait()
-        # Analyze the core dump to find the offset
-        core = p.corefile
-        offset = cyclic_find(core.read(core.rsp, 8), n=8)
+    return offset
 
 
-# Print the offset
-log.success("Offset: {}".format(offset))
-#clear core files
-os.system('rm -rf ./core.*')
+def main():
+    if len(sys.argv) != 2:
+        log.warn("Usage: python script_name.py path_to_binary")
+        sys.exit(1)
+
+    binary_path = sys.argv[1]
+    
+    offset = find_offset(binary_path)
+
+    if offset is not None:
+        log.success("Offset: {}".format(offset))
+    else:
+        log.error("Offset detection failed.")
+
+    # Clean up core files
+    os.system('rm -rf ./core.*')
+
+if __name__ == "__main__":
+    main()
