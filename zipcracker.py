768957796331b223cd8245e18a8ed068b9634772
m3ez <supakiad.satuwan@gmail.com>
2023-09-13 01:39:30 +0700

    update zip cracker

--- zipcracker.py
+++ zipcracker.py
@@ -2,29 +2,32 @@
 import zipfile
 import argparse
 
+
+
 def main():
-	parser = argparse.ArgumentParser(description="Zipfile password cracker using a brute-force dictionary attack.")
-	parser.add_argument("zipfile", help="The zip file to be cracked")
-	parser.add_argument("wordlist", help="The wordlist file")
-	args = parser.parse_args()
-	zipfilename = args.zipfile
-	dictionary = args.wordlist
-	
-	password = None
-	zip_file = zipfile.ZipFile(zipfilename)
-	with open(dictionary, 'r') as f:
-		print("[?] Cracking ZIP file: "+ zipfilename )
-		for line in f.readlines():
-			password = line.strip('\n')
-			try: 
-				if extract_zip(zip_file, password):
-					zip_file.extractall(pwd=password)
-			except:
-				pass
-	print("\033[31m[+] Password: "+ password)
-	print(f"\033[32m[+] Decompress with command: unzip -P '{password}' {zipfilename}")
-	print("\033[36")
-	exit()
+    parser = argparse.ArgumentParser(description="Zipfile password cracker using a brute-force dictionary attack.")
+    parser.add_argument("zipfile", help="The zip file to be cracked")
+    parser.add_argument("wordlist", help="The wordlist file")
+    args = parser.parse_args()
+    zipfilename = args.zipfile
+    dictionary = args.wordlist
+    
+    password = None
+    zip_file = zipfile.ZipFile(zipfilename)
+    with open(dictionary, 'r', encoding='latin-1') as f:  # Specify the correct encoding (e.g., 'latin-1')
+        print("[?] Cracking ZIP file: "+ zipfilename )
+        for line in f.readlines():
+            password = line.strip('\n')
+            try: 
+                zip_file.extractall(pwd=password)
+                password = 'Password found: %s' % password
+                break
+            except:
+                pass
+    print("\033[31m[+] Password: "+ password)
+    print(f"\033[32m[+] Decompress with command: unzip -P '{password}' {zipfilename}")
+    print("\033[36")
+    exit()
 
 if __name__ == '__main__':
 	main()
